bc3c1a0fb24450b0f18fa231d5ed27113ff42467
Prokuma <prokuma@prokuma.dev>
2023-09-24 16:26:57 +0900

    [Update] バージョン合わせ

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/Prokuma/PLAccounting-WebFrontend.git
 cp .env.docker PLAccounting-WebFrontend/.env
 cd PLAccounting-WebFrontend
-git checkout tags/alpha-v0.1.0
+git checkout tags/alpha-v0.2.0
 npm install
 npm run build
 mv ./build ../html
